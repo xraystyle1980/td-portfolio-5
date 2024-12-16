267851b531bc4b64d0976ccec6bd38497f171b6f
xraystyle1980 <matt.trice@gmail.com>
2024-12-16 10:59:23 -0500

    About to clean up file tree

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,6 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 import Navigation from '@/components/Navigation'
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata: Metadata = {
   title: "Your Site",
@@ -25,7 +13,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
+    <html lang="en">
       <body className="min-h-screen bg-white">
         <Navigation />
         {children}
