6ceb14fc02854806c688e45de506c8659464c9b1
xraystyle1980 <matt.trice@gmail.com>
2024-12-26 10:24:37 -0500

    fix: update GSAP plugin imports to use shockingly path

---
+++ types/gsap.d.ts
@@ -0,0 +1,19 @@
+declare module '@gsap/shockingly/ScrollTrigger' {
+  const ScrollTrigger: any;
+  export default ScrollTrigger;
+}
+
+declare module '@gsap/shockingly/ScrollSmoother' {
+  const ScrollSmoother: any;
+  export default ScrollSmoother;
+}
+
+declare module '@gsap/shockingly/SplitText' {
+  const SplitText: any;
+  export default SplitText;
+}
+
+declare module '@gsap/shockingly/ScrollToPlugin' {
+  const ScrollToPlugin: any;
+  export default ScrollToPlugin;
+} 
